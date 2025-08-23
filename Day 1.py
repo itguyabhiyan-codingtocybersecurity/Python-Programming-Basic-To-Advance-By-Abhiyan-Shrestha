d4c83ae2d6de5f18042be0babb63234a11d915a1
Abhiyan Shrestha <itguyabhiyan@gmail.com>
2025-08-23 16:10:37 +0545

    Learned How To Use Print Statement
    Single Line Code Used in Python Programming

---
+++ Day 1.py
@@ -0,0 +1,7 @@
+# Print Statement For Output
+# Single Line Code
+# Done by Abhiyan Shrestha
+
+print("Hello World !") # Single Line Syntax For Printing Hello World Output
+
+############################################################################
