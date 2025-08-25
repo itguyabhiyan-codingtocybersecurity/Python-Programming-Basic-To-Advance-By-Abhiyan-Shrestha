93d4b95534c30b89f00e2fbfa962d3ce9b2ba595
Abhiyan Shrestha <itguyabhiyan@gmail.com>
2025-08-25 12:33:12 +0545

    Learned how to use variables and Using Identifier
    Learned Variables and Identifer cases - Camel Case, Pascal Case, and Snake Case

---
+++ Day 2.py
@@ -0,0 +1,53 @@
+# Variable - Store data in program
+# Line by Line Checking
+# Done by Abhiyan Shrestha
+
+###############################################################################
+
+a = 'Hello World' # Variable value is set to "Hello World"
+
+print (a); # Calling variable "a" from print statement for output "Hello World"
+print (a); # Calling variable "a" from print statement for output "Hello World"
+print (a); # Calling variable "a" from print statement for output "Hello World"
+print (a); # Calling variable "a" from print statement for output "Hello World"
+print (a); # Calling variable "a" from print statement for output "Hello World"
+
+################################################################################
+
+a = 'Python Programming' # Variable value is set to "Python Programming"
+
+print (a); # Calling variable 'a' from print statement for output "Python Programming"
+print (a); # Calling variable 'a' from print statement for output "Python Programming"
+print (a); # Calling variable 'a' from print statement for output "Python Programming"
+print (a); # Calling variable 'a' from print statement for output "Python Programming"
+print (a); # Calling variable 'a' from print statement for output "Python Programming"
+
+#####################################################################################
+
+a = 'Hello' # Variable name is set to "Hello" first
+a = 'World' # then again variable name is set to "World" so output will be World
+
+print (a); #calling variable 'a' from print statement and output will "World" as Python programming Works Line by Line
+print (a); #calling variable 'a' from print statement and output will "World" as Python programming Works Line by Line
+print (a); #calling variable 'a' from print statement and output will "World" as Python programming Works Line by Line
+print (a); #calling variable 'a' from print statement and output will "World" as Python programming Works Line by Line
+print (a); #calling variable 'a' from print statement and output will "World" as Python programming Works Line by Line
+
+########################################################################################
+
+# Using Identifier
+
+# 1 Camel Case
+
+helloworldvariable = 'Camel Case'
+print(helloworldvariable); 
+
+# 2 Pascal Case
+
+HelloWorldVariable = 'Pascal Case'
+print (HelloWorldVariable); 
+
+# 3 Snake Case
+
+hello_world_variable = 'Snake Case'
+print (hello_world_variable); 
