6b7f02013543c87cf88a404b52d04a2affdbf14b
Abhiyan Shrestha <itguyabhiyan@gmail.com>
2025-09-12 12:31:27 +0545

    Day 5 Coding Python by Abhiyan Shrestha
    String Conversion: You can convert a list of integers to a string. The example shows str([1,2,3,4,5]) which results in the string representation of the list.
    
    Integer Conversion:
    
    You can convert a float (like 34.5) to an integer, which truncates the decimal part.
    
    You can convert a string of digits (like '23456') to an integer. However, a string containing non-digit characters (like '23456 a') will cause an error during this conversion.
    
    Float Conversion: You can convert an integer or a float to a float. The script demonstrates float(23) and float(23.5).
    
    List, Tuple, and Set Conversion: You can convert a list to a tuple and back to a list. The script shows converting the list [1,2,3,4,5] into a tuple and a list again.
    
    Dictionary Conversion: You can convert a tuple of key-value pairs into a dictionary. The example (('a','World'),('b','Hello')) is converted into a dictionary.
    
    Boolean Conversion: A non-empty string like 'true' can be converted to a boolean value, which results in True.

---
+++ Day 5.py
@@ -0,0 +1,51 @@
+# Type Conversion
+# String (any data type)
+# Coding is Done by Abhiyan Shrestha
+
+r = [1,2,3,4,5]
+s = str (r)
+print (s)
+
+# Integer
+
+a = 34.5 # Converting float to integer
+b = int (a)
+print (b)
+
+c = '23456' # String Value ho
+d = int (c)
+print (d/2)
+
+e = '23456 a' # Not valid
+f = int (e)
+print (f)
+
+# Float
+
+g = 23
+h = float (g)
+print (h)
+
+i = 23.5
+j = float (i)
+print (j)
+
+# List, tuple & set
+
+k = [1,2,3,4,5]
+l = tuple (k)
+m = list (k)
+print (l)
+print (m)
+
+# Dictionary 
+
+n = (('a','World'),('b','Hello'))
+o = dict (n)
+print (o)
+
+#Boolean 
+
+p = 'true'
+q = bool (p)
+print (q)
